15ab175e6459173b94b07a221ac2ce9f8158bbe5
Mr.Jack <alejandro.olp@gmail.com>
2023-11-24 15:20:16 -0600

    i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,59 @@
+export const languages = {
+  es: 'Español',
+  en: 'English'
+}
+
+export const defaultLang = 'es';
+
+export const ui = {
+  es: {
+    'profession': 'Ingeniero en Sistemas Computacionales',
+    'about_me': 'Sobre mí',
+    'home': 'Inicio',
+    'contact_me': 'Contáctame',
+    'library': 'Biblioteca',
+    'download': 'Descargar',
+    'about_me_1': 'Soy un programador apasionado, me gusta mantenerme en movimiento y seguir aprendiendo todos los dias, me interesa ayudar a encontrar soluciones eficientes o crearlas.\nSoy extremadamente curioso, me interesa la seguridad informática y la involucro en todo lo que hago.',
+    'about_me_2': 'Actualmente vivo en: México',
+    'technologies': 'Tecnologías',
+    'year': 'año',
+    'years': 'años',
+    'experience': 'Experiencia',
+    'projects': 'Proyectos',
+    'others': 'Otros',
+    'software_engineer': 'Ingeniero de Software',
+    'senior_developer': 'Programador Senior',
+    'web_developer': 'Desarrollador Web',
+    'current': 'actualidad',
+    'footer_1': 'Página creada con',
+    'footer_2': 'código fuente',
+    'footer_3': 'por',
+    'pdf': 'https://alexomar.com/docs/CVAlejandrolopez.pdf',
+    'keywords': 'Innovación, Seguridad, Tecnología, Desarrollo, Infraestructura, Control, Soporte, Consultoría, Sistemas, Coordinación, Dirección, Planeación y Realización de Proyectos, Actualidad, Gadgets, Liderazgo'
+  },
+  en: {
+    'profession': 'Computer Systems Engineer',
+    'about_me': 'About me',
+    'home': 'Home',
+    'contact_me': 'Contact me',
+    'library': 'Library',
+    'download': 'Download',
+    'about_me_1': 'I am a passionate programmer, I like to keep moving and continue learning every day, I am interested in helping and find efficient solutions or creating them.\nI am extremely curious, I am interested in computer security and I involve it in everything I do.',
+    'about_me_2': 'Currently living in: Mexico',
+    'technologies': 'Technologies',
+    'year': 'year',
+    'years': 'years',
+    'experience': 'Experience',
+    'projects': 'Projects',
+    'others': 'Others',
+    'software_engineer': 'Software Engineer',
+    'senior_developer': 'Senior Developer',
+    'web_developer': 'Web Developer',
+    'current': 'current',
+    'footer_1': 'Website created with',
+    'footer_2': 'source code',
+    'footer_3': 'by',
+    'pdf': 'https://alexomar.com/docs/CVAlejandrolopez_en.pdf',
+    'keywords': 'Innovation, Security, Technology, Development, Infrastructure, Management, Support, Consultancy, Systems, Teams Coordination, Supervision, Planning, Project realization, Trends, Gadgets, Leadership'
+  }
+} as const
